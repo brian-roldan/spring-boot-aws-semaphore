6c10821b0ca7655de62218af3c338c05e71a09f5
Brian Roldan <broldan.saztrek@gmail.com>
2018-03-22 09:35:13 +0800

    added new service to check if it works now

---
+++ src/main/java/io/broldan/SampleSample.java
@@ -0,0 +1,14 @@
+package io.broldan;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleSample {
+
+	@GetMapping("/holla")
+	public String helloWorld() {
+		return "{Holla:at Yo boi}";
+	}
+	
+}
